0fd75871cb2a58169a22e11c10d2c021d3382262
john <natonmwangi@gmail.com>
2022-04-21 12:04:36 +0300

    third commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://dashboard.heroku.com/apps/pacific-mountain-82390/callback_url.php';  
+  $CallBackURL = 'https://pacific-mountain-82390.herokuapp.com/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
